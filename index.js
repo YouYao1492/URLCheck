2e18d0c5572eb2528356f77f46fbfc7cd1705e73
YouYao1492 <13131492@me.mcu.edu.tw>
2026-06-12 14:20:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dayjs = require("dayjs");
 const path = require("path");
 
 // --- 1. 設定區域 ---
-const CONFIG_SPREADSHEET_ID = "1gBhgTPvrYSGujOl6vKVk769NuvchsvJ3EfTLzFNECvE"; // 請替換為你的主控試算表 ID
+const CONFIG_SPREADSHEET_ID = "1TDY7sx4qipdaMupf-MZgrgQRr2FKjZ4KuBXMHjMT9a8"; // 請替換為你的主控試算表 ID
 const CONFIG_SHEET_NAME = "配置";
 const KEYWORD = "your_keyword_here"; // 請替換為你的關鍵字
 const TIMEOUT_MS = 5000;
